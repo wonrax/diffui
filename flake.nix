e92d4107114aa1b9bcf24954ba2af7f94a717781
wonrax <hahuylonghai2012@gmail.com>
2026-05-13 14:33:58 +0700

    feat: nix package output for diffui

--- flake.nix
+++ flake.nix
@@ -12,82 +12,128 @@
       nixpkgs,
       flake-utils,
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        inherit (pkgs) lib;
+    let
+      perSystem = flake-utils.lib.eachDefaultSystem (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          inherit (pkgs) lib;
 
-        linuxRuntimeLibs = with pkgs; [
-          libx11
-          libxcb
-          libxcursor
-          libxkbcommon
-          wayland
-          vulkan-loader
-        ];
+          linuxRuntimeLibs = with pkgs; [
+            libx11
+            libxcb
+            libxcursor
+            libxkbcommon
+            wayland
+            vulkan-loader
+          ];
 
-        runtimeFonts = with pkgs; [
-          fontconfig
-          cascadia-code
-        ];
+          darwinBuildInputs = [ pkgs.apple-sdk_15 ];
 
-        darwinBuildInputs = [ pkgs.apple-sdk_15 ];
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          packages =
-            with pkgs;
-            [
-              cargo
-              clippy
-              nixfmt
-              rust-analyzer
-              rustc
-              rustfmt
-            ]
-            ++ lib.optionals stdenv.isLinux (
-              [
-                pkg-config
-              ]
-              ++ runtimeFonts
-              ++ linuxRuntimeLibs
-            );
+          diffui = pkgs.rustPlatform.buildRustPackage {
+            pname = "diffui";
+            version = "0.1.0";
 
-          buildInputs = lib.optionals pkgs.stdenv.isDarwin darwinBuildInputs;
+            src = lib.cleanSourceWith {
+              src = ./.;
+              filter =
+                path: type:
+                let
+                  base = baseNameOf path;
+                in
+                base != "target" && base != "result" && base != ".direnv";
+            };
 
-          RUST_BACKTRACE = "1";
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+              # iced is pulled from git in Cargo.toml; let nix fetch by the
+              # commits already pinned in Cargo.lock instead of forcing us to
+              # maintain an outputHashes table.
+              allowBuiltinFetchGit = true;
+            };
 
-          shellHook =
-            lib.optionalString pkgs.stdenv.isLinux ''
-              diffui_runtime_libs="${pkgs.lib.makeLibraryPath linuxRuntimeLibs}"
-              existing_ld_library_path="''${LD_LIBRARY_PATH#:}"
-              existing_xdg_data_dirs="''${XDG_DATA_DIRS#:}"
+            nativeBuildInputs = [ pkgs.pkg-config ] ++ lib.optionals pkgs.stdenv.isLinux [ pkgs.makeWrapper ];
 
-              if [ -n "$existing_ld_library_path" ]; then
-                export LD_LIBRARY_PATH="$diffui_runtime_libs:$existing_ld_library_path"
-              else
-                export LD_LIBRARY_PATH="$diffui_runtime_libs"
-              fi
+            buildInputs =
+              lib.optionals pkgs.stdenv.isLinux (linuxRuntimeLibs ++ [ pkgs.fontconfig ])
+              ++ lib.optionals pkgs.stdenv.isDarwin darwinBuildInputs;
 
-              if [ -n "$existing_xdg_data_dirs" ]; then
-                export XDG_DATA_DIRS="${pkgs.lib.makeSearchPath "share" runtimeFonts}:$existing_xdg_data_dirs"
-              else
-                export XDG_DATA_DIRS="${pkgs.lib.makeSearchPath "share" runtimeFonts}"
-              fi
+            doCheck = false;
 
-              export FONTCONFIG_FILE="${pkgs.fontconfig.out}/etc/fonts/fonts.conf"
-              export FONTCONFIG_PATH="${pkgs.fontconfig.out}/etc/fonts"
-
-              unset diffui_runtime_libs existing_ld_library_path existing_xdg_data_dirs
-            ''
-            + ''
-              export ICED_BACKEND="''${ICED_BACKEND:-wgpu}"
-              export WGPU_POWER_PREF="''${WGPU_POWER_PREF:-none}"
+            postInstall = lib.optionalString pkgs.stdenv.isLinux ''
+              wrapProgram $out/bin/diffui \
+                --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath linuxRuntimeLibs}"
             '';
-        };
 
-        formatter = pkgs.nixfmt;
-      }
-    );
+            meta = {
+              description = "Native GUI diff viewer for jj and git";
+              homepage = "https://github.com/haiha/diffui";
+              mainProgram = "diffui";
+              platforms = lib.platforms.unix;
+            };
+          };
+        in
+        {
+          packages = {
+            default = diffui;
+            diffui = diffui;
+          };
+
+          apps.default = {
+            type = "app";
+            program = "${diffui}/bin/diffui";
+          };
+
+          devShells.default = pkgs.mkShell {
+            packages =
+              with pkgs;
+              [
+                cargo
+                clippy
+                nixfmt
+                rust-analyzer
+                rustc
+                rustfmt
+              ]
+              ++ lib.optionals stdenv.isLinux (
+                [
+                  pkg-config
+                  fontconfig
+                ]
+                ++ linuxRuntimeLibs
+              );
+
+            buildInputs = lib.optionals pkgs.stdenv.isDarwin darwinBuildInputs;
+
+            RUST_BACKTRACE = "1";
+
+            shellHook =
+              lib.optionalString pkgs.stdenv.isLinux ''
+                diffui_runtime_libs="${pkgs.lib.makeLibraryPath linuxRuntimeLibs}"
+                existing_ld_library_path="''${LD_LIBRARY_PATH#:}"
+
+                if [ -n "$existing_ld_library_path" ]; then
+                  export LD_LIBRARY_PATH="$diffui_runtime_libs:$existing_ld_library_path"
+                else
+                  export LD_LIBRARY_PATH="$diffui_runtime_libs"
+                fi
+
+                unset diffui_runtime_libs existing_ld_library_path
+              ''
+              + ''
+                export ICED_BACKEND="''${ICED_BACKEND:-wgpu}"
+                export WGPU_POWER_PREF="''${WGPU_POWER_PREF:-none}"
+              '';
+          };
+
+          formatter = pkgs.nixfmt;
+        }
+      );
+    in
+    perSystem
+    // {
+      overlays.default = final: prev: {
+        diffui = self.packages.${final.system}.default;
+      };
+    };
 }
